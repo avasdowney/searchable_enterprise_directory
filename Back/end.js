c561c13a26374c19818204e6cc38ceef36c4a4a4
Jacob <Jacob@jacob>
2023-07-13 16:57:33 -0400

    ayeeee

--- Back/end.js
+++ Back/end.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var dao = require("./server");
+var dao = require("./server"); 
+
+// import { registerUser } from './server';
 
 // server app
 var app = express();
+// console.log("hello")
+// console.log("hi")
 
 //Parse JSON body
 app.use(bodyParser.json());
 
-//
-app.get("/register", (req, res) => {
+// get employees
+app.get("/Employee", async (req, res) => {
     //button event 
-    dao.registerUser('getplanets',{}, (result)=>{
+    await dao.findEmployees((result)=>{
         console.log("result: " + result);
         res.send(result);
     })
 });
+
+// get employee by ID
+app.get("/Employee", async (req, res) => {
+    //button event 
+    await dao.findEmployees(req.params.id, (result)=>{
+        console.log("result: " + result);
+        res.send(result);
+    })
+});
+
+// start the rest service
+var port = 4000;
+console.log('service opening on port: ' + port)
+app.listen(port);
