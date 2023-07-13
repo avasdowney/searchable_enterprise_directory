08279c20e0d5cdf5a5016418685d1e9779a6da54
Noah Ramdial <test@test.com>
2023-07-13 16:32:48 -0400

    got a query working

--- Back/end.js
+++ Back/end.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var dao = require("./server");
+var dao = require("./server"); 
+
+// import { registerUser } from './server';
 
 // server app
 var app = express();
+// console.log("hello")
+// console.log("hi")
 
 //Parse JSON body
 app.use(bodyParser.json());
 
-//
-app.get("/register", (req, res) => {
+// get employee by ID
+app.get("/:id", async (req, res) => {
     //button event 
-    dao.registerUser('getplanets',{}, (result)=>{
+    await dao.findEmpID(req.params.id, (result)=>{
         console.log("result: " + result);
         res.send(result);
     })
 });
+
+// start the rest service
+var port = 4000;
+console.log('service opening on port: ' + port)
+app.listen(port);
